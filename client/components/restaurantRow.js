5753e90e8b3b834c57b25439d1701ec01119201d
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-13 13:48:37 -0400

    feat: adds relevant data from yelp api to restaurant items

--- client/components/restaurantRow.js
+++ client/components/restaurantRow.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import yelpLogo from '../../public/Yelp_trademark_RGB.png'
 
 const RestaurantRow = props => {
-  const {name} = props.info
+  const {name, rating, price, url} = props.info
+  const reviewCount = props.info.review_count
   return (
     <div>
-      {name}
+      <span>{name}</span>
+      <span>{rating}</span>
+      <span>{price}</span>
+      <span>
+        {reviewCount}
+        &nbsp;reviews
+      </span>
+      <span>
+        <a href={url}>
+        View on Yelp
+          {/* <img src={yelpLogo} alt="yelp" className="yelpLogo"/> */}
+        </a>
+
+      </span>
     </div>
   )
 }
