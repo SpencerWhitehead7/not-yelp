412cb66e380cddd8ae0658caa268eccb18145de6
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-15 19:33:28 -0400

    feat: restaurant rows now display type of restaurant

--- client/components/restaurantRow.js
+++ client/components/restaurantRow.js
@@ -2,11 +2,13 @@ import React from 'react'
 // import yelpLogo from '../../public/Yelp_trademark_RGB.png'
 
 const RestaurantRow = props => {
-  const {name, rating, price, url} = props.info
+  const {name, rating, price, url, categories} = props.info
+  const categoriesStr = categories.map(category => category.title).join(`, `)
   return (
     <div className="uk-width-1-1 uk-grid uk-grid-small uk-child-width-1-1 uk-card uk-card-default uk-card-small uk-card-hover">
       <div className="uk-card-header">
         <h4>{name}</h4>
+        <p>{categoriesStr}</p>
       </div>
       <div className="uk-card-body uk-grid uk-grid-collapse uk-child-width-1-3">
         <p className="uk-text-center">{rating}</p>
