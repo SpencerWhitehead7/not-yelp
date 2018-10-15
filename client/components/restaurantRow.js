120a073c4ada4a5ee04c14ec2d4de570202441f4
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-15 19:25:14 -0400

    refactor: restaurant rows now card style rows

--- client/components/restaurantRow.js
+++ client/components/restaurantRow.js
@@ -4,16 +4,20 @@ import React from 'react'
 const RestaurantRow = props => {
   const {name, rating, price, url} = props.info
   return (
-    <div className="uk-width-1-1 uk-grid uk-grid-small uk-child-width-1-4">
-      <span className="uk-text-left">{name}</span>
-      <span className="uk-text-center">{rating}</span>
-      <span className="uk-text-center">{price}</span>
-      <span className="uk-text-right">
-        <a href={url}>
+    <div className="uk-width-1-1 uk-grid uk-grid-small uk-child-width-1-1 uk-card uk-card-default uk-card-small uk-card-hover">
+      <div className="uk-card-header">
+        <h4>{name}</h4>
+      </div>
+      <div className="uk-card-body uk-grid uk-grid-collapse uk-child-width-1-3">
+        <p className="uk-text-center">{rating}</p>
+        <p className="uk-text-center">{price}</p>
+        <p className="uk-text-center">
+          <a href={url}>
         View on Yelp
-          {/* <img src={yelpLogo} alt="yelp" className="yelpLogo"/> */}
-        </a>
-      </span>
+            {/* <img src={yelpLogo} alt="yelp" className="yelpLogo"/> */}
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
